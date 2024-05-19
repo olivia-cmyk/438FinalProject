d0dbd0d4fee5d78e9ba9767dfcf87c4cf4070b6d
olivia-cmyk <yaushi20@uw.edu>
2024-05-18 22:43:19 -0700

    Commit 3 Editing

--- notes-app/src/CardPreviewComponent.js
+++ notes-app/src/CardPreviewComponent.js
@@ -5,23 +5,74 @@ import {useState, useEffect} from 'react';
 
 function CardPreviewComponent({front, back, cardId}) {
     const [isFlipped, setIsFlipped] = useState(false); //false = front, true = back
+    const [currentFront, setFront] = useState(front)
+    const [amEditingFront, setAmEditingFront] = useState(false); //false = not editing
+    const [currentBack, setBack] = useState(back)
+    const [amEditingBack, setAmEditingBack] = useState(false);
 
-    const handleCardClick = () => {
+
+    const handleCardClick = () => { //will trigger card to "flip" in css
         setIsFlipped(!isFlipped); //changes true->false and false->true
       };
 
+    const handleDoubleClickFront = () => { //when doubleclick happens, trigger editing
+        setAmEditingFront(true);
+    }
+
+    const handleFrontChange = (event) => { //when text on changes, set currentFront to changed text
+        setFront(event.target.value);
+    }
+
+    const handleFrontBlur = () => { //when clicking out, exit editing mode.
+        setAmEditingFront(false);
+    }
+
+    const handleDoubleClickBack = () => { //when doubleclick happens, trigger editing
+        setAmEditingBack(true);
+    }
+
+    const handleBackChange = (event) => { //when text on changes, set currentBack to changed text
+        setBack(event.target.value);
+    }
+
+    const handleBackBlur = () => { //when clicking out, exit editing mode.
+        setAmEditingBack(false);
+    }
+
 
     return(
-    <div className="card-container">
+    <div className="middle-container">
         {/* class name changes based on whether card flipped is true or false */}
-        <div className={`flashcard ${isFlipped ? 'flipped':''}`} onClick={handleCardClick}>
-            <div className="card-front">
-                <h3>{front}</h3>
-            </div>
-            <div className="card-back">
-                <h3>{back}</h3>
+        <div className="card-container">
+            <div className={`flashcard ${isFlipped ? 'flipped':''}`}>
+                <div className="card-front">
+                    {/*will change front b/w input and actual text on double click*/}
+                    {amEditingFront ?
+                    (<input type="text"
+                    className="input"
+                    value={currentFront}
+                    onChange={handleFrontChange}
+                    onBlur={handleFrontBlur}
+                    autoFocus></input>)
+                    : (<h3 onDoubleClick={handleDoubleClickFront}>{currentFront}</h3>)
+                    }
+                </div>
+                <div className="card-back">
+                    {/*will change back b/w input and actual text on double click*/}
+                    {amEditingBack ?
+                    (<input type="text"
+                    className="input"
+                    value={currentBack}
+                    onChange={handleBackChange}
+                    onBlur={handleBackBlur}
+                    autoFocus></input>)
+                    : (<h3 onDoubleClick={handleDoubleClickBack}>{currentBack}</h3>)
+                    }
+                </div>
+
             </div>
         </div>
+        <button className="flip-card-button" onClick={handleCardClick}>Flip Card</button>
     </div>
     );
 }
